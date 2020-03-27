6fcdf65dd9651872ebd75d3b49f161d6e8272a12
Lindsey Barbee-Vargas <lbarbeev@redhat.com>
2020-03-27 15:00:10 -0400

    Added fa icons and code selection timeout

--- _javascripts/clipboard.js
+++ _javascripts/clipboard.js
@@ -1,1 +1,29 @@
 // This file runs the Clipboard.js functionality //
+document.querySelectorAll('div.listingblock').forEach((codeblock, index) => {
+  codeblock.insertAdjacentHTML("beforebegin", "<p><span class='clipboard-button fa fa-clipboard'></span></p>");
+  document.getElementsByTagName('pre')[index].setAttribute('id',`hello${index}`);
+});
+
+document.querySelectorAll('span.clipboard-button').forEach((copybutton, index) => {
+  copybutton.setAttribute('data-clipboard-target',`#hello${index}`);
+});
+
+var clipboard = new ClipboardJS('.clipboard-button');
+
+clipboard.on('success', function(e) {
+    console.info('Action:', e.action);
+    console.info('Text:', e.text);
+    console.info('Trigger:', e.trigger);
+    e.trigger.classList.toggle("fa-clipboard");
+    e.trigger.classList.toggle("fa-check");
+    setTimeout(function(){
+      e.clearSelection();
+      e.trigger.classList.toggle("fa-clipboard");
+      e.trigger.classList.toggle("fa-check");
+    }, 2000);
+});
+
+clipboard.on('error', function(e) {
+    console.error('Action:', e.action);
+    console.error('Trigger:', e.trigger);
+});
